489c06a08e100c04a9daf4c0be704d30ed3fef73
Mohammedvd13 <asadshaikh1@outlook.com>
2021-11-19 10:37:50 +0100

    afffff

--- eindopdracht/quiz.py
+++ eindopdracht/quiz.py
@@ -1,10 +1,10 @@
-print('Welcome to Mohammed Shaikh his Quiz')
+print('Welkom bij Mohammed Shaikh zijn Quiz')
 answer=input('Are you ready to play the Quiz ? (yes/no) :')
 score=0
 total_questions=3
  
 if answer.lower()=='yes':
-    answer=input('Question 1: What is your Favourite programming language?')
+    answer=input('Question 1: Wat is je lievelingscodetaal?')
     if answer.lower()=='python':
         score += 1
         print('correct')
@@ -12,21 +12,21 @@ if answer.lower()=='yes':
         print('Wrong Answer :(')
  
  
-    answer=input('Question 2: Do you know how to use python? ')
+    answer=input('Question 2: Weet je hoe je Python gebruikt? ')
     if answer.lower()=='yes':
         score += 1
         print('correct')
     else:
         print('Wrong Answer :(')
  
-    answer=input('Question 3: do you like to code?>')
+    answer=input('Question 3: Vind je het leuk om te coderen?>')
     if answer.lower()=='':
         score += 1
         print('correct')
     else:
         print('Wrong Answer :(')
  
-print('Thankyou for Playing this small quiz game, you attempted',score,"questions correctly!")
+print('BEdankt voor je tijd om me quiz te spelen...',score,"questions correctly!")
 mark=(score/total_questions)*100
 print('Marks obtained:',mark)
 print('BYE!')
